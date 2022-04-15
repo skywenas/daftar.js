e74a8940b39b7efa7c315a32bb4c041f9532eda1
SkyDEV <100242647+skywenas@users.noreply.github.com>
2022-04-15 11:16:36 +0800

    Create daftar.js
    Daftar Link Kecamatan Se Kota Bitung

---
+++ daftar.js
@@ -0,0 +1,38 @@
+//Script_By_WENSAH
+var select = document.getElementById('camat');
+document.getElementById("daftar").onclick = function(){
+    var value = select.value;
+    if (value == "Ranowulu")
+    {
+        window.location.href = "Ranowulu.php";
+    }
+    else if (value == "Matuari")
+    {
+        window.location.href = "Matuari.php";
+    }
+    else if (value == "Girian")
+    {
+        window.location.href = "Girian.php/";
+    }
+	else if (value == "Madidir")
+    {
+        window.location.href = "Madidir.php";
+    }
+	else if (value == "Maesa")
+    {
+        window.location.href = "Maesa.php";
+    }
+	else if (value == "Aertembaga")
+    {
+        window.location.href = "Aertembaga.php";
+    }
+	else if (value == "Lesat")
+    {
+        window.location.href = "Lesat.php";
+    }
+	else if (value == "Lembut")
+    {
+        window.location.href = "Lembut.php";
+    }
+	
+};
